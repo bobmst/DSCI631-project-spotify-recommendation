10efd60c949ee5a153381a9d46f93d4a2c3c39ae
bobmst <bobmst1573@gmail.com>
2023-04-29 13:47:08 -0400

    init

---
+++ data_acquisition/spotify_info.py
@@ -0,0 +1,20 @@
+# TODO
+
+"""
+Acquire additional data of music infromation from spotify api using the spotify uri
+"""
+
+
+def get_track_info(uri):
+    # TODO
+    pass
+
+
+def get_artist_info(uri):
+    # TODO
+    pass
+
+
+def get_album_info(uri):
+    # TODO
+    pass
