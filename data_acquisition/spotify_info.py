5e8f21c2ab61d8937b167b03555addf5ad2c48cf
bobmst <bobmst1573@gmail.com>
2023-04-30 15:36:46 -0400

    update EDA with attributes

--- data_acquisition/spotify_info.py
+++ data_acquisition/spotify_info.py
@@ -1,20 +1,69 @@
-# TODO
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import secrests
+import pandas as pd
+from tqdm import tqdm
+from retry import retry
+import asyncio
+
+
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=secrests.SPOTIFY_CLIENT_ID, client_secret=secrests.SPOTIFY_CLIENT_SECRET
+)
+sp_client = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 """
 Acquire additional data of music infromation from spotify api using the spotify uri
 """
 
 
-def get_track_info(uri):
-    # TODO
-    pass
+def test():
+    print("Testing")
 
 
-def get_artist_info(uri):
-    # TODO
-    pass
+@retry(delay=30, tries=4)
+async def get_track_info(uri):
+    info = sp_client.audio_features(uri)[0]
+    # drop unnessasary features
+    info.pop("type", None)
+    info.pop("id", None)
+    info.pop("track_href", None)
+    info.pop("analysis_url", None)
+    info.pop("time_signature", None)
+
+    return info
 
 
-def get_album_info(uri):
-    # TODO
-    pass
+@retry(delay=30, tries=4)
+async def get_artist_info(uri):
+    info = sp_client.artist(uri)
+    # drop unnessasary features
+    info.pop("external_urls", None)
+    info.pop("href", None)
+    info.pop("id", None)
+    info.pop("images", None)
+    info.pop("name", None)
+
+    return info
+
+
+@retry(delay=30, tries=4)
+async def get_album_info(uri):
+    info = sp_client.album(uri)
+    # drop unnessasary features
+    info.pop("album_group", None)
+    info.pop("album_type", None)
+    info.pop("artists", None)
+    info.pop("available_markets", None)
+    info.pop("copyrights", None)
+    info.pop("external_ids", None)
+    info.pop("external_urls", None)
+    info.pop("href", None)
+    info.pop("id", None)
+    info.pop("images", None)
+    info.pop("name", None)
+    info.pop("tracks", None)
+    info.pop("type", None)
+
+    return info
